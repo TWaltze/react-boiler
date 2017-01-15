1a043a62927788f9cb3670970414c268063653da
Tyler Waltze <tylerwaltze@gmail.com>
2017-01-15 00:54:41 -0500

    Switch some spacing to tabs

--- server.js
+++ server.js
@@ -9,20 +9,20 @@ var app = express();
 var compiler = webpack(config);
 
 app.use(devMiddleware(compiler, {
-  publicPath: config.output.publicPath,
-  historyApiFallback: true,
+	publicPath: config.output.publicPath,
+	historyApiFallback: true,
 }));
 
 app.use(hotMiddleware(compiler));
 
 app.get('*', function (req, res) {
-  res.sendFile(path.resolve(config.output.path), 'index.html');
+	res.sendFile(path.resolve(config.output.path), 'index.html');
 });
 
 app.listen(3000, function (err) {
-  if (err) {
-    return console.error(err);
-  }
+	if (err) {
+		return console.error(err);
+	}
 
-  console.log('Listening at http://localhost:3000/');
+	console.log('Listening at http://localhost:3000/');
 });
